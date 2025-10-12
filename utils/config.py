a6cc5f789fc39aab25809621c9813dc6d648c797
Zhong Mingjie <412052582@qq.com>
2025-10-12 16:08:01 +0800

    Auth.login

--- utils/config.py
+++ utils/config.py
@@ -1,13 +1,18 @@
 import pathlib as pl
 import tomllib as tl
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Annotated
 
 CONFIG_PATH = pl.Path(__file__).parents[1] / 'custom_config.toml'
 class DatabaseConfig(BaseModel):
     url: str
 
+class SessionIDConfig(BaseModel):
+    ttl: Annotated[int, Field(ge=0)]
+
 class CustomConfig(BaseModel):
     database: DatabaseConfig
+    sessionid: SessionIDConfig
 
 def init():
     with open(CONFIG_PATH, 'rb') as f:
--- utils/database.py
+++ utils/database.py
@@ -11,7 +11,16 @@ class User(Base):
     nickname = sa.Column(sa.String(64), nullable=False)
     passwd_hash = sa.Column(sa.String(64), nullable=False)
     created_at = sa.Column(sa.DateTime, nullable=False)
+    logged_in_at = sa.Column(sa.DateTime)
     deleted_at = sa.Column(sa.DateTime)
+
+class SessionID(Base):
+    __tablename__ = 'sessionid'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    session_id = sa.Column(sa.String(32), unique=True)
+    user_id = sa.Column(sa.Integer)
+    expired_at = sa.Column(sa.DateTime)
 
 engine = sa.create_engine(url=config.database.url)
 Base.metadata.create_all(engine)
